f9367c255dc9757d9c4b61d60f7d837c924857b8
TheDudeFromCI <thedudefromci@gmail.com>
2022-04-03 22:46:52 -0700

    Removed pathfinder in favor of individual actions.

--- src/main/java/me/ci/project/mio/api/IMinecraftAgent.java
+++ src/main/java/me/ci/project/mio/api/IMinecraftAgent.java
@@ -1,7 +1,7 @@
 package me.ci.project.mio.api;
 
 import org.joml.Vector3f;
-import me.ci.project.mio.api.pathfinding.IPathfinder;
+import me.ci.project.mio.api.actions.IAction;
 
 /**
  * A bridge for interacting with the Minecraft agent entity within the game.
@@ -10,18 +10,31 @@ import me.ci.project.mio.api.pathfinding.IPathfinder;
  */
 public interface IMinecraftAgent
 {
-    /**
-     * Gets the current position of the agent.
-     *
-     * @return The current world position.
-     */
-    Vector3f getPosition();
+	/**
+	 * Gets the action that this agent is currently performing.
+	 *
+	 * @return The agent's current action, or null if the agent is not doing
+	 *         anything.
+	 */
+	IAction getAction();
 
-    /**
-     * Gets the pathfinder service for this agent.
-     *
-     * @return The pathfinder.
-     */
-    IPathfinder getPathfinder();
+	/**
+	 * Gets the current position of the agent.
+	 *
+	 * @return The current world position.
+	 */
+	Vector3f getPosition();
 
+	/**
+	 * Assigns a new action to this agent and begins execution. If this agent is
+	 * already in the middle of a running action, the current action is cancelled.
+	 *
+	 * @param action
+	 *            - The action to execute.
+	 *
+	 * @throws IllegalStateException
+	 *             - If the agent is currently performing and action that cannot be
+	 *             cancelled.
+	 */
+	void setAction(IAction action);
 }
